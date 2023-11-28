3eab09deed4705f8c1b9b5940e0a3e574fc33d04
SoraxDubbing <sorax5.r23@gmail.com>
2023-11-28 21:29:05 +0100

    Adapt to gradle remove all other sub project drop Residence support drop other minecraft version support (1.12.2 only)

---
+++ src/main/java/com/nisovin/magicspells/util/wrappers/CraftItemStackWrapper.java
@@ -0,0 +1,53 @@
+package com.nisovin.magicspells.util.wrappers;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import de.slikey.effectlib.util.ReflectionUtils;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
+
+public class CraftItemStackWrapper {
+	
+	public static Class<?> targetClass = CraftItemStack.class; //TODO get the class from effectLib
+	
+	public static Field craftItemStackHandleField;
+	public static Method asNMSCopyMethod;
+	public static Method asCraftCopyMethod;
+	
+	public static Object newInstance() {
+		try {
+			return targetClass.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
+	static {
+		try {
+			craftItemStackHandleField = targetClass.getDeclaredField("handle");
+			craftItemStackHandleField.setAccessible(true);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			asNMSCopyMethod = ReflectionUtils.getMethod(targetClass, "asNMSCopy", net.minecraft.server.v1_12_R1.ItemStack.class);
+			asNMSCopyMethod.setAccessible(true);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			asCraftCopyMethod = targetClass.getDeclaredMethod("asCraftCopy", net.minecraft.server.v1_12_R1.ItemStack.class);
+			asCraftCopyMethod.setAccessible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
